9d625281d1dd348ea572369f6fadd3f85f7fec59
15797938417 <15797938417@163.com>
2019-07-17 13:55:22 +0800

    20190717

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -12,30 +12,102 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        jumpHeight: 0,
+        //主角跳跃高度
+        jumpDuration: 0,
+        //主角跳跃持续时间
+        maxMoveSpeed: 0,
+        //最大移动速度
+        accel: 0,
+        //加速度
+
+        jumpAudio: {
+            default: null,
+            type: cc.AudioClip
+        },
     },
 
+    setJumpAction: function() {
+        //跳跃上升
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionInOut());
+        //下落
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCircleActionIn());
+        var callback = cc.callFunc(this.playJumpSound, this);
+
+        //不断重复
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown, callback));
+    },
+
+    playJumpSound: function() {
+        cc.audioEngine.playEffect(this.jumpAudio, false);
+    },
+
+    onKeyDown(event) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = true;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = true;
+                break;
+        }
+    },
+
+    onKeyUp(event) {
+        switch (event.keyCode) {
+            case cc.macro.KEY.a:
+                this.accLeft = false;
+                break;
+            case cc.macro.KEY.d:
+                this.accRight = false;
+                break;
+        }
+    },
+
+
+    onLoad: function() {
+        //初始化跳跃动作
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+        //加速度方向开关
+        this.accLeft = false;
+        this.accRight = false;
+        this.xSpeed = 0;
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
+
+    onDestroy() {
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    },
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
-    start () {
+    start() {
 
     },
 
-    // update (dt) {},
+    update: function(dt) {
+
+            if (this.accLeft) {
+                this.xSpeed -= this.accel * dt;
+            } else if (this.accRight) {
+                this.xSpeed += this.accel * dt;
+            }
+            if (Math.abs(this.xSpeed) > this.maxMoveSpeed) {
+                this.xSpeed = this.maxMoveSpeed * this.xSpeed / Math.abs(this.xSpeed);
+            }
+
+            this.node.x += this.xSpeed * dt;
+
+            if (this.node.x >= 441) {
+                this.node.x = 441;
+            }
+            if (this.node.x <= -441) {
+                this.node.x = -441;
+            }
+        }
+        // update (dt) {},
 });
